1e91f504d5a5a1cf700b2b020f41a558c6962326
Bruno Scopelliti <bru.scopelliti@gmail.com>
2014-04-25 10:08:38 +0200

    fix

--- app/assets/scripts/modules/todooo/config/routes.js
+++ app/assets/scripts/modules/todooo/config/routes.js
@@ -21,36 +21,7 @@ window.app.config(['$routeProvider', '$httpProvider', function($routeProvider, $
 
 	$routeProvider.otherwise({ redirectTo: '/404' });
 
-	/* Set XHR request interceptor */
-	$httpProvider.interceptors.push(['$q', function($q) {
-
-		var serialize = function(obj, prefix) {
-			var str = [];
-			for(var p in obj) {
-				var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
-				str.push(typeof v == "object" ? serialize(v, k) : encodeURIComponent(k) + "=" + encodeURIComponent(v));
-			}
-			return str.join("&");
-		}
-
-		return {
-			request: function(config) {
-				if (config.method === 'POST' && config.data && typeof config.data === 'object') {
-					config.data = serialize(config.data);
-				}
-				return config || $q.when(config);
-			}
-		};
-
-	}]);	
-
 }]);
 
 
-window.app.run(['$rootScope', '$http', function($root, $http) {
-
-	//
-	
-	$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8;";
-
-}]);
+// window.app.run(['$rootScope', '$http', function($root, $http) {}]);
